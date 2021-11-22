f987507139eaa67dc8a7af0fe63d435689344960
lamerson28 <82220715+rozay28@users.noreply.github.com>
2021-11-22 16:15:29 -0500

    Added email functionality

--- src/client/components/Wishlist.jsx
+++ src/client/components/Wishlist.jsx
@@ -1,11 +1,57 @@
 import  React from "react";
+import { useState, useEffect, useRef } from 'react';
 import { Link } from "react-router-dom";
 
 const Wishlist = () => {
+    const [email, setEmail] = useState('');
+    // const [message, setMessage] = useState('');
+    const [parentEmail, setParentEmail] = useState('');
     
+
+    const sendEmail =  () => {
+         Email.send({
+            Host: "smtp.gmail.com",
+            Username: "santaprep2021@gmail.com",
+            Password: "Codesmith2021",
+            To: parentEmail,
+            From: "santaprep2021@gmail.com",
+            Subject: "2021 Wishlist",
+            Body: email,
+          }).then(
+              alert("mail sent successfully")
+          ).catch(err);
+    }
+
+    function fakeAlert() {
+        alert('Draft Saved!')
+      }
+
+    // const firstUpdate = useRef(true);
+    // useEffect(()=> {
+    //   // Jump out of callback if this is first render
+    //   if(firstUpdate.current) {
+    //     firstUpdate.current = false;
+    //     return;
+    //   }
+      
+    //   Email.send({
+    //     Host: "smtp.gmail.com",
+    //     Username: "santaprep2021@gmail.com",
+    //     Password: "Codesmith2021",
+    //     To: parentEmail,
+    //     From: "santaprep2021@gmail.com",
+    //     Subject: "2021 Wishlist",
+    //     Body: email,
+    //   })
+    //     .then(() => {
+    //       alert("mail sent successfully")
+    //     });
+    // }, [message]);
+
+
     return (
         <div className="wishlist">
-            <form action="/reviews" method="post">
+            <form>
                 <div className="row">
                     <div className="input-field">
                     <label htmlFor="title" className="to">Santa's Email ➠ </label>
@@ -13,21 +59,21 @@ const Wishlist = () => {
                     </div>
                     <div className="input-field">
                     <label htmlFor="title" className="to">Parent's Email ➠ </label>
-                    <input type="text" className="toSanta" id="title" name="title" ></input>
+                    <input type="text" className="toSanta" id="title" name="title" onChange ={e=> {setParentEmail(e.target.value)}}></input>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-field">
                     <h5>🎅 Write your wishlist to Santa here! 🎁</h5>
-                        <textarea name="body" id="body" className="textarea" placeholder="What should Santa bring?" cols="90" rows="25"></textarea>
+                        <textarea name="body" id="body" className="textarea" placeholder="What should Santa bring?" cols="90" rows="25" onChange={e => {setEmail(e.target.value)}}></textarea>
                     </div>
                 </div>
                 <div className="row">
-                    <input type="submit" value="Save draft" className="submit"></input>
-                    <input type="submit" value="Send to Santa Clause" className="submit"></input>
+                    <input type="button" value="Save draft" onClick={fakeAlert} className="submit"></input>
+                    <input type="submit" value="Send to Santa Clause" className="submit" onClick={e=> {sendEmail()}}></input>
                     <button type="submit">
-                <Link to="/Dashboard">Draft</Link>
-            </button>
+                      <Link to="/Dashboard">Draft</Link>
+                    </button>
                 </div>
             </form>
             <div className="btn btnbottom">
